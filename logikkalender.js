dd0d30b0bf730a23950fbd8c5a0bca9a13abe6fe
Niko <nikolerch@t-online.de>
2024-07-29 14:15:07 +0200

    first commit

---
+++ logikkalender.js
@@ -0,0 +1,25 @@
+// let monateÜberschrift = ["Januar", "Februar","März", "April", "Mai", "Juni", "Juli", "August", "September","Oktober", "November","Dezember"]
+// let monateNameÜberschrift = monate[month -1]
+// document.getElementById("MonatÜberschrift").innerText = monateNameÜberschrift
+// let jahresÜberschrift = document.getElementById("JahresÜberschrift").innerText = year;
+// const wochentage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag","Freitag", "Samstag","Sonntag"];
+// const table = document.getElementById("calendar-table")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
